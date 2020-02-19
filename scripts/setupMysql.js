9413ff635329b95b8f4e1c4821bd65649ebdf619
ALMaclaine <almaclaine@gmail.com>
2020-02-18 21:51:25 -0800

    add scripts

---
+++ scripts/setupMysql.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const {
+  query,
+  connectionWrapper
+} = require('../src/db');
+
+const sql = fs.readFileSync(path.join(__dirname, '../src/db/nodejs_sql_api/setup.sql'), 'utf-8')
+              .split("\n")
+              .filter(e => e !== "")
+              .join("");
+
+async function setup() {
+  const out = await connectionWrapper((conn) => query(conn, sql));
+  await connectionWrapper((conn) => conn.end());
+  return out;
+}
+
+setup().then(e => {
+  console.log(e); process.exit();
+}).catch(e => console.log(e));
