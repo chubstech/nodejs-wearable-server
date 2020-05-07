e358ce90caae7c8fb7a084021789cde8204b1add
Alessandro Maclaine <almaclaine@gmail.com>
2020-05-07 16:46:42 -0700

    Update index.js

--- src/server/routes/api/index.js
+++ src/server/routes/api/index.js
@@ -4,6 +4,6 @@ const noiseObservationRoutes = require(path.join(__dirname, './noise_observation
 const userRoutes = require(path.join(__dirname, './users'));
 
 router.use("/noise_observation", noiseObservationRoutes);
-router.use("/user", userRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
