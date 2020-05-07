60b12e596eaa54990c7cd20c9e8729cca597663b
Alessandro Maclaine <almaclaine@gmail.com>
2020-05-07 16:47:39 -0700

    Update users.js

--- src/server/routes/api/users.js
+++ src/server/routes/api/users.js
@@ -3,14 +3,6 @@ const path = require('path');
 const { querier, connectionWrapper } = require(path.join(__dirname, '../../../db'));
 
 router.get('/', function(req, res) {
-  // This get endpoint retrieves all noise level data points by user_id
-  const { id: user_id } = req.params;
-
-  if(!user_id) {
-    res.status(400);
-    res.json({ error: "Must send id" });
-    return;
-  }
   (async function() {
     const doc = await querier.noiseObservation.get();
     const out = await connectionWrapper(doc);
