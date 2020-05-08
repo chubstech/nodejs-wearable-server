1f67387f9d7e9c90a35d50b0251034542824a657
Alessandro Maclaine <almaclaine@gmail.com>
2020-05-07 17:35:39 -0700

    Update proxy_ini.js

--- src/db/nodejs_sql_api/proxy_ini.js
+++ src/db/nodejs_sql_api/proxy_ini.js
@@ -23,7 +23,6 @@ function recBuilder(recObj) {
       const params = recObj.params.split(',');
       const ownKeys = JSON.stringify(params);
       return function (obj) {
-        if(obj === null) return;
         const objKeys = JSON.stringify(Object.keys(obj));
         if (ownKeys !== objKeys) throw new Error("Keys do not match");
         return format(recObj.q)(obj);
