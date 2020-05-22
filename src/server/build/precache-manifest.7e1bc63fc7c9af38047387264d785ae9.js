f4b56c1fb39fca8325a3b51ffd721605b70e2174
Alessandro Maclaine <almaclaine@gmail.com>
2020-05-22 14:24:19 -0700

    add client

---
+++ src/server/build/precache-manifest.7e1bc63fc7c9af38047387264d785ae9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c6eea4b7048800dd9bd8dceb284afc",
+    "url": "/host-website/index.html"
+  },
+  {
+    "revision": "dd787620f96726f8cd9f",
+    "url": "/host-website/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c916453041b9301451af",
+    "url": "/host-website/static/css/main.0cb463cd.chunk.css"
+  },
+  {
+    "revision": "dd787620f96726f8cd9f",
+    "url": "/host-website/static/js/2.b146cae4.chunk.js"
+  },
+  {
+    "revision": "77c52c67682ebaf3c87f021fd687577f",
+    "url": "/host-website/static/js/2.b146cae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c916453041b9301451af",
+    "url": "/host-website/static/js/main.e1086401.chunk.js"
+  },
+  {
+    "revision": "ed6d7d981c029cbd95dd",
+    "url": "/host-website/static/js/runtime-main.3fe7bfe5.js"
+  }
+]);
--- src/server/server.js
+++ src/server/server.js
@@ -5,18 +5,16 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const routes = require(path.join(__dirname, "./routes"));
 app.use(routes);
 
-app.get('/', function (req, res, next) {
-  res.send('<div>Shhhh</div>');
-})
-
-app.get("*", function(req, res) {
-  res.redirect(302, '/');
-})
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname, '/build/index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
